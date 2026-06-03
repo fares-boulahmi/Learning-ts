7c1d4d23d35fc50e0b8c6384e7af55c992c6cef7
fares-boulahmi <boulahmifares@gmail.com>
2026-06-03 11:48:02 +0100

    into to pizza app : create CompleteOrder+ testing

--- into to pizza app/script.js
+++ into to pizza app/script.js
@@ -6,15 +6,16 @@ const menu = [
 ]
 
 let cashInRegister = 100
-const orderQueue = []
+const orderQueue = [];
+let orderId = 1;
 
 /**
  * Challenge: Add a utility function "addNewPizza" that takes a pizza object
  * and adds it to the menu.
  */ 
 
-function addNewPizza(name , price ) {
-  return  menu.push({name, price});
+function addNewPizza(order) {
+  return  menu.push(order);
     
 }
 
@@ -43,9 +44,9 @@ function placeOrder(name) {
         }
     } 
     cashInRegister += orderedPizza.price ;
-    orderQueue.push({pizza: orderedPizza , status : "ordered"});
-    console.log(`the pizza was ordered is ${name} and the price is ${orderedPizza.price} and the cashinorder after order become ${cashInRegister} and the order  is the return value next .`);
-    return console.log(orderQueue[orderQueue.length - 1]);
+    orderQueue.push({id: orderId++ ,  pizza: orderedPizza , status : "ordered"});
+    console.log(`the pizza was ordered is ${name} and the price is ${orderedPizza.price} and the cashinorder after order become ${cashInRegister} and the order  is the return value next ${orderQueue[orderQueue.length - 1]}.`);
+    return orderQueue[orderQueue.length - 1];
 
     
 }
@@ -61,3 +62,47 @@ placeOrder("Pepperoni");
         return newOrder
     }
 */
+
+
+
+
+/**
+ * Challenge: write another utility function, completeOrder, that takes an orderId as a parameter
+ * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
+ * return the found order from the function.
+ * 
+ * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
+ */
+
+function completeOrder(orderId) {
+    let orderCompleted ;
+    for (let i = 0; i < orderQueue.length; i++) {
+        if (orderQueue[i].id == orderId) {
+            orderQueue[i].status = "completed" ;
+            orderCompleted = orderQueue[i];
+        }
+    }
+    return orderCompleted;
+}
+
+
+// Instructor Code 
+/**
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
+*/
+
+
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder(1)
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order que:", menu)
