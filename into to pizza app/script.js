35a2a1d4b0a9fed0d20f253b157abae4d4e4bf9a
fares-boulahmi <boulahmifares@gmail.com>
2026-06-03 11:11:27 +0100

    add addNewPizza function

---
+++ into to pizza app/script.js
@@ -0,0 +1,19 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+
+function addNewPizza(name , price ) {
+  return  menu.push({name, price});
+    
+}
