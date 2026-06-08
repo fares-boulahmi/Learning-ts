e71d15b039a893459f7752a2d684f82fb6adb65e
fares-boulahmi <boulahmifares@gmail.com>
2026-06-08 17:45:28 +0100

    complete Tenzies recreate on TS challenge by scrimba under link https://scrimba.com/learn-typescript-c03c/~06g6

---
+++ Tenzies/src/Die.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from "react/jsx-runtime";
+
+type DieProps = {
+  key: string;
+  value: number;
+  isHeld: boolean;
+  hold: () => void;
+};
+export default function Die(props: DieProps): JSX.Element {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+    >
+      {props.value}
+    </button>
+  );
+}
